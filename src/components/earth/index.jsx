b3ca5d701abe66b3477d8bf1ceb8df29bdb11795
Thomas Martinez <thomas.martinez@igt.com>
2023-06-27 23:01:54 -0400

    added Clouds

--- src/components/earth/index.jsx
+++ src/components/earth/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useLoader } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
+import * as THREE from 'three';
 
 import EarthDayMap from '../../assets/textures/8k_earth_daymap.jpg';
 import EarthNormalMap from '../../assets/textures/8k_earth_normal_map.jpg';
@@ -17,6 +18,16 @@ export function Earth(props) {
   return (
     <>
       <ambientLight intensity={0.5} />
+      <mesh>
+        <sphereGeometry args={[1.005, 32, 32]} />
+        <meshPhongMaterial
+          map={cloudsMap}
+          opacity={0.4}
+          depthWrite={true}
+          transparent={true}
+          side={THREE.DoubleSide}
+        />
+      </mesh>
       <mesh>
         <sphereGeometry args={[1, 32, 32]} />
         <meshPhongMaterial specularMap={specularMap} />
