79f331409bb8b41b053e753fcaceea571dbe1cdf
Thomas Martinez <thomas.martinez@igt.com>
2023-06-27 22:50:03 -0400

    added texture images and added to earth component

--- src/components/earth/index.jsx
+++ src/components/earth/index.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import { useLoader } from '@react-three/fiber';
+
+import EarthDayMap from '../../assets/textures/8k_earth_daymap.jpg';
+import EarthNormalMap from '../../assets/textures/8k_earth_normal_map.jpg';
+import EarthSpecularMap from '../../assets/textures/8k_earth_specular_map.jpg';
+import EarthCloudsMap from '../../assets/textures/8k_earth_clouds.jpg';
+import { TextureLoader } from 'three';
 
 export function Earth(props) {
+  const [colorMap, normalMap, specularMap, cloudsMap] = useLoader(
+    TextureLoader,
+    [EarthDayMap, EarthNormalMap, EarthSpecularMap, EarthCloudsMap]
+  );
+
   return (
     <>
       <ambientLight intensity={0.5} />
       <mesh>
         <sphereGeometry args={[1, 32, 32]} />
-        <meshPhongMaterial color='red' />
+        <meshPhongMaterial specularMap={specularMap} />
+        <meshStandardMaterial map={colorMap} normalMap={normalMap} />
       </mesh>
     </>
   );
