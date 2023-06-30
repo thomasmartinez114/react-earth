fac4fa6c726227ea630804451eeb847e0b651d21
Thomas Martinez <thomas.martinez@igt.com>
2023-06-29 22:29:46 -0400

    added topSection text above earth

--- src/components/earth/index.jsx
+++ src/components/earth/index.jsx
@@ -22,14 +22,14 @@ export function Earth(props) {
   useFrame(({ clock }) => {
     const elapsedTime = clock.getElapsedTime();
 
-    earthRef.current.rotation.y = elapsedTime / 6;
-    cloudsRef.current.rotation.y = elapsedTime / 6;
+    earthRef.current.rotation.y = elapsedTime / 10;
+    cloudsRef.current.rotation.y = elapsedTime / 10;
   });
 
   return (
     <>
       {/* <ambientLight intensity={0.5} /> */}
-      <pointLight color='#f6f3ea' position={[2, 0, 2]} intensity={1.2} />
+      <pointLight color='#f6f3ea' position={[2, 0, 5]} intensity={1.2} />
       <Stars
         radius={300}
         depth={60}
@@ -38,7 +38,7 @@ export function Earth(props) {
         saturation={0}
         fade={true}
       />
-      <mesh ref={cloudsRef}>
+      <mesh ref={cloudsRef} position={[0, 0, 3]}>
         <sphereGeometry args={[1.005, 32, 32]} />
         <meshPhongMaterial
           map={cloudsMap}
@@ -48,7 +48,7 @@ export function Earth(props) {
           side={THREE.DoubleSide}
         />
       </mesh>
-      <mesh ref={earthRef}>
+      <mesh ref={earthRef} position={[0, 0, 3]}>
         <sphereGeometry args={[1, 32, 32]} />
         <meshPhongMaterial specularMap={specularMap} />
         <meshStandardMaterial
@@ -57,14 +57,14 @@ export function Earth(props) {
           metalness={0.4}
           roughness={0.7}
         />
-        <OrbitControls
+        {/* <OrbitControls
           enableZoom={true}
           enablePan={true}
           enableRotate={true}
           zoomSpeed={0.6}
           panSpeed={0.5}
           rotateSpeed={0.4}
-        />
+        /> */}
       </mesh>
     </>
   );
